da87a77fee37a7c6b7708bf665cf0e19664d07ad
PaulRoss22 <95063507+PaulRoss22@users.noreply.github.com>
2023-02-11 17:34:16 -0600

    added some more functionality

--- Week5Test/Controllers/Week5Lecture.cs
+++ Week5Test/Controllers/Week5Lecture.cs
@@ -19,6 +19,8 @@ namespace Week5Test.Controllers
         }
         String returnString(string input)
         {
+            //Here I log the character at position zero
+            System.Diagnostics.Debug.WriteLine(input[0]);
             return input;
         }
     }
